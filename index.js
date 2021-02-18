2c0babf1b9b341b101e9e37f47f2df8360af49df
aikobdibs <aiko.schurmann@gmail.com>
2021-02-18 13:50:35 +0100

    6

--- index.js
+++ index.js
@@ -10,13 +10,23 @@ let play2 = document.querySelector(".play2");
 let duration;
 let TimeStart = document.querySelector(".time-start");
 let TimeEnd = document.querySelector(".time-end");
-
+let artist = document.querySelector(".artist");
+let artistValue = artist.innerHTML.replace(/&amp;/g, "&");
 let song1 = new Audio("./song1.mp3");
+let player;
+let videotime;
+let tag = document.createElement("script");
+let firstScriptTag = document.getElementsByTagName("script")[0];
+let id = "M7lc1UVf-VE";
+let title = document.querySelector(".title");
+tag.src = "https://www.youtube.com/iframe_api";
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 expand.addEventListener("click", expandsection);
 collapse.addEventListener("click", collapsesection);
 timeBar2.addEventListener("input", updateTimebar);
 song1.addEventListener("loadeddata", loadeddata);
+window.onresize = displayUpdate;
 
 function expandsection() {
   controlBar.style.height = "100%";
@@ -31,56 +41,108 @@ function collapsesection() {
   controlBarExpanded.style.display = "none";
 }
 function togglepause() {
-  if (song1.paused) {
-    song1.play();
+  if (player.getPlayerState() != 1) {
+    //song1.play();
+    player.playVideo();
     play.classList = "fas fa-pause play";
     play2.classList = "fas fa-pause play2";
   } else {
-    song1.pause();
     play.classList = "fas fa-play play";
     play2.classList = "fas fa-play play2";
+    player.pauseVideo();
   }
 }
 function togglepause2() {
-  if (song1.paused) {
-    song1.play();
+  if (player.getPlayerState() === 5) {
+    player.playVideo();
     play2.classList = "fas fa-pause play2";
     play.classList = "fas fa-pause play";
   } else {
-    song1.pause();
+    player.stopVideo();
     play2.classList = "fas fa-play play2";
     play.classList = "fas fa-play play";
   }
 }
 function updateTimebar() {
-  song1.currentTime = (timeBar2.value / 1000) * song1.duration;
+  player.seekTo((timeBar2.value / 1000) * duration);
 }
 function displaytime() {
-  var min = Math.floor(song1.currentTime / 60);
+  var min = Math.floor(videotime / 60);
   var secs =
-    Math.floor(song1.currentTime % 60) < 10
-      ? "0" + Math.floor(song1.currentTime % 60)
-      : Math.floor(song1.currentTime % 60);
+    Math.floor(videotime % 60) < 10
+      ? "0" + Math.floor(videotime % 60)
+      : Math.floor(videotime % 60);
 
-  var min1 = Math.floor(song1.duration / 60);
+  var min1 = Math.floor(duration / 60);
   var sec1 =
-    Math.floor(song1.duration % 60) < 10
-      ? "0" + Math.floor(song1.duration % 60)
-      : Math.floor(song1.duration % 60);
+    Math.floor(duration % 60) < 10
+      ? "0" + Math.floor(duration % 60)
+      : Math.floor(duration % 60);
 
   TimeStart.innerHTML = min + ":" + secs;
 
   TimeEnd.innerHTML = min1 + ":" + sec1;
 }
 function loadeddata() {
-  duration = song1.duration;
-  song1.addEventListener("timeupdate", (event) => {
-    timeBar.style.width = (song1.currentTime / song1.duration) * 100 + "%";
-    timeBar2.value = (song1.currentTime / song1.duration) * 1000;
-
-    displaytime();
-  });
+  displaytime();
+  timeBar.style.width = (videotime / duration) * 100 + "%";
+  timeBar2.value = (videotime / duration) * 1000;
 
   play.addEventListener("click", togglepause);
   play2.addEventListener("click", togglepause2);
 }
+
+function displayUpdate() {
+  let maxCharacters = Math.floor((screen.width - 165) / 7 - 4);
+
+  artistValue.length > maxCharacters
+    ? (artist.innerHTML = artistValue.slice(0, maxCharacters) + "...")
+    : (artist.innerHTML = artistValue.slice(0, maxCharacters));
+}
+
+function updateInfo() {
+  artist.innerHTML = player.getVideoData().author;
+  title.innerHTML = player.getVideoData().title;
+}
+function onPlayerReady(event) {
+  duration = player.getDuration();
+  player.playVideo();
+  player.seekTo(0);
+
+  function updateTime() {
+    loadeddata();
+    var oldTime = videotime;
+    if (player && player.getCurrentTime) {
+      videotime = player.getCurrentTime();
+    }
+    if (videotime !== oldTime) {
+    }
+  }
+  timeupdater = setInterval(updateTime, 250);
+}
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "0",
+    width: "0",
+    videoId: "j59qQ7YWLxw",
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+}
+displayUpdate();
+
+setInterval(() => {
+  id = "xd2bKrDzMTg";
+  player.destroy();
+
+  player = new YT.Player("player", {
+    height: "0",
+    width: "0",
+    videoId: id,
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+}, 1000000000);
